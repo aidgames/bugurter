293e00111bde224af8079801f6c47887290db08f
aidgamesa <ya.ivanov1302@yandex.ru>
2023-07-04 15:12:06 +0700

    switch to menu.xml

--- app/src/main/java/ru/undefined/bugurter/MainActivity.java
+++ app/src/main/java/ru/undefined/bugurter/MainActivity.java
@@ -3,6 +3,7 @@ package ru.undefined.bugurter;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.content.ClipboardManager;
 import android.content.ClipData;
 import android.widget.Toast;
@@ -14,11 +15,6 @@ import ru.undefined.bugurter.BugurtHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 0xff00 + item id
-    private static int menulast = 0xff00;
-    private static final int MenuItem_COPYBUGURT = ++menulast;
-    private static final int MenuItem_CONVERTBUGURT = ++menulast;
-    private static final int MenuItem_CONVERTTEXT = ++menulast;
     private EditText edittext;
 
     @Override
@@ -30,29 +26,28 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-        menu.add(Menu.NONE, MenuItem_COPYBUGURT, Menu.NONE, getString(R.string.menu_copy_bugurt));
-        menu.add(Menu.NONE, MenuItem_CONVERTBUGURT, Menu.NONE, getString(R.string.menu_convert_bugurt));
-        menu.add(Menu.NONE, MenuItem_CONVERTTEXT, Menu.NONE, getString(R.string.menu_convert_text));
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        if (item.getItemId()==MenuItem_COPYBUGURT) {
+        if (item.getItemId()==R.id.menu_copy_bugurt) {
             ClipboardManager clipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
             clipboard.setPrimaryClip(ClipData.newPlainText("Bugurt",
                 BugurtHelper.toBugurt(edittext.getText().toString())
             ));
             Toast.makeText(this, getString(R.string.copied), 0)
                 .show();
-        } else if (item.getItemId()==MenuItem_CONVERTBUGURT) {
+        } else if (item.getItemId()==R.id.menu_convert_bugurt) {
             edittext.setText(
                 BugurtHelper.toBugurt(    
                     edittext.getText().toString()
                 )
             );
-        } else if (item.getItemId()==MenuItem_CONVERTTEXT) {
+        } else if (item.getItemId()==R.id.menu_convert_text) {
             edittext.setText(
                 BugurtHelper.toText(    
                     edittext.getText().toString()
